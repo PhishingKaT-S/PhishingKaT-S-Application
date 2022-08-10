d724589889d4111eee9cc87c9a96bcf20b0e8e24
Yooisae <dbdlto@naver.com>
2022-08-10 16:00:10 +0900

    complete info_check_page

--- lib/kat_pages/info_check_page.dart
+++ lib/kat_pages/info_check_page.dart
@@ -1,4 +1,11 @@
+/**
+ * writer: 유이새
+ * Date: 2022.08.10
+ * Description: 털린 내 정보 찾기 서비스 페이지 webview
+ */
+
 import 'package:flutter/cupertino.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class InfoCheckPage extends StatefulWidget {
   const InfoCheckPage({Key? key}) : super(key: key);
@@ -10,6 +17,13 @@ class InfoCheckPage extends StatefulWidget {
 class _InfoCheckPageState extends State<InfoCheckPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SafeArea(
+      child: WebView(
+        initialUrl: 'https://kidc.eprivacy.go.kr/search/issueVerify.do',
+        javascriptMode: JavascriptMode.unrestricted,
+        gestureNavigationEnabled: true,
+        userAgent: "random",
+      ),
+    );
   }
 }
