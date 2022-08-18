e83fb26e10f5d90fba87a446518f65df763c7a3d
김진일 <jiniljeil1@naver.com>
2022-08-18 21:58:17 +0900

    flutter switch

--- lib/kat_pages/setting_page.dart
+++ lib/kat_pages/setting_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_switch/flutter_switch.dart';
 
 import '../Theme.dart';
 import '../kat_widget/kat_appbar_back.dart';
@@ -12,6 +13,8 @@ class settingPage extends StatefulWidget {
 }
 
 class _settingPageState extends State<settingPage> {
+  List<bool> status = [false, true, false, true] ;
+  List<String> setting_names = ['피싱관련 뉴스 알림', '이벤트 알림', '알림 미리보기', '푸시알림'] ;
 
   Widget _title() {
     return Container(
@@ -24,13 +27,60 @@ class _settingPageState extends State<settingPage> {
     ) ;
   }
 
+  Widget _flutter_switch(int index) {
+
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: MediaQuery.of(context).size.width * 0.1, vertical: 5),
+      width: MediaQuery.of(context).size.width ,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text(setting_names[index]),
+
+          (index < 3) ? (
+            FlutterSwitch(
+              height: 30,
+              showOnOff: true,
+              activeTextColor: AppTheme.white,
+              inactiveTextColor: AppTheme.white,
+              value: status[index],
+              onToggle: (val) {
+                setState(() {
+                  status[index] = val;
+                });
+              },
+            )
+          ) :
+          (
+            InkWell(
+              onTap: () {} ,
+              child: Center(
+                child: Container(
+                  padding: EdgeInsets.only(top: 3, bottom: 3),
+                  width: 90,
+                  height: 30,
+                  decoration: BoxDecoration(
+                    color: AppTheme.blueLineChart,
+                    borderRadius: BorderRadius.all(Radius.circular(15)),
+                  ),
+                  child: Text('소리+진동', style: TextStyle(color: AppTheme.white), textAlign: TextAlign.center,)
+                )
+              )
+            )
+          )
+        ]
+      )
+    );
+  }
+
   Widget _buttons() {
     return Container(
       padding: EdgeInsets.only(top: 30),
       child: Column(
-        children: [
-          /// Toggle Button
-        ],
+        children: List.generate(setting_names.length, (index) {
+          return _flutter_switch(index);
+        })
       ),
     );
   }
