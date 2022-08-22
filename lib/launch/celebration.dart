16caadf767254da20c66dde8b319d5abf440cf57
Jiwon <skwldnjs1@naver.com>
2022-08-22 15:17:42 +0900

    8/22

--- lib/launch/celebration.dart
+++ lib/launch/celebration.dart
@@ -4,12 +4,10 @@
    discription 0.6 celebration for joinning
  */
 
-
 import 'package:flutter/material.dart';
 
 import '../Theme.dart';
 import '../kat_widget/launch_appbar.dart';
-
 
 /*
 * 이 화면이 보이면 3초후 꺼지고 진짜 home으로 돌아감
@@ -17,39 +15,44 @@ import '../kat_widget/launch_appbar.dart';
 * */
 
 class CeleBration extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    final _height = (MediaQuery.of(context).size.height~/5);
     return Scaffold(
-      bottomNavigationBar: Container(child:
-          Image.asset('assets/images/launch_end.png', fit: BoxFit.cover),
-          width: double.infinity,
-          height: 3*_height.toDouble(),
-      ),//이미지 불러오기
-      appBar: certification_appbar(Colors.white, Colors.white),
-      body: SingleChildScrollView(
-        padding: EdgeInsets.fromLTRB(20, 10, 20, 0),
-        child:
-          Container(
+        body: Container(
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              mainAxisAlignment: MainAxisAlignment.end,
               children: <Widget>[
-                Align(
-              alignment: Alignment.centerLeft,
-                      child: Text(
-                      '당신과,\n당신의 소중한 이웃들이\n피싱으로부터 안전할 수 있게', style: AppTheme.title,
+                Container(
+
+                  padding: EdgeInsets.fromLTRB(MediaQuery.of(context).size.width*0.1, MediaQuery.of(context).size.height *0.1, 20, 0),
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    '당신과,\n당신의 소중한 이웃들이\n피싱으로부터 안전할 수 있게',
+                    style: AppTheme.title,
                   ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Container(
+                  padding: EdgeInsets.fromLTRB(MediaQuery.of(context).size.width*0.1, 0, 20, MediaQuery.of(context).size.width*0.1),
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    '피싱캣S는 모두가 안심하고\n소통할 수 있는 세상을 만듭니다',
+                    style: AppTheme.body1,
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                      child: Image.asset('assets/images/launch_end.png',
+                          fit: BoxFit.fitWidth),
+                      width: double.infinity,
                     ),
-                SizedBox(height: 20,),
-                    Align(alignment: Alignment.centerLeft,
-                        child: Text('피싱캣S는 모두가 안심하고\n소통할 수 있는 세상을 만듭니다', style: AppTheme.body1,), ),
-                
+                ),
               ],
             ),
-          ),//당신과 당신의
-
-      )
-
-    );
+          ), //당신과 당신의
+        );
   }
 }
