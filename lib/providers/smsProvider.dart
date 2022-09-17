bf898857061e60600901cbcfc85eba7a54626145
Yooisae <dbdlto@naver.com>
2022-09-17 18:13:10 +0900

    update

--- lib/providers/smsProvider.dart
+++ lib/providers/smsProvider.dart
@@ -1,11 +1,15 @@
+import 'package:contacts_service/contacts_service.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:mysql1/mysql1.dart';
 import 'package:intl/intl.dart';
+import 'package:permission_handler/permission_handler.dart';
 import '../db_conn.dart';
 import 'launch_provider.dart';
 
 class SmsProvider with ChangeNotifier{
   late List<SmsInfo> _smsList;
+  List<String> _user_contact = [];
+
 
   SmsProvider(){
     _smsList = [];
@@ -23,10 +27,38 @@ class SmsProvider with ChangeNotifier{
       _smsList.add(SmsInfo(name: temp[0], phone: temp[1], date: temp[2], body: temp[3]));
     }
     notifyListeners();
+    //_makeScore();
   }
+
+  // Future<int> _makeScore() async{
+  //   if(await getContacts()){
+  //     for(int i = 0; i < _smsList.length ; i++){
+  //       if()
+  //     }
+  //   }
+  //
+  // }
+
 
   int getSmsLength(){
     return _smsList.length;
+  }
+
+  Future<bool> getContacts() async {
+    var status = await Permission.contacts.status;
+    if (status.isGranted) {
+      var temp = await ContactsService.getContacts();
+      for (int i = 0; i < temp.length; i++) {
+        print("phone number: ");
+        _user_contact.add(temp[i].phones!.first.value.toString());
+      }
+      return true;
+    } else if (status.isDenied) {
+      Permission.contacts.request();
+      return false;
+    }
+    return false;
+
   }
 }
 
