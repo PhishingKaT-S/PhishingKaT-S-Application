604d8afb32afb89beed3191b46a702e40d438391
Yooisae <63344032+Yooisae@users.noreply.github.com>
2022-11-09 01:45:48 +0900

    Yooisae (#48)
    * test
    
    * initial data
    
    * info_check update
    
    * navigator edit and provider init
    
    * update menu home page
    
    * real
    
    * delete main init
    
    * update provider
    
    * error edit
    
    * update
    
    * smsprovier
    
    * error fix
    
    * update
    
    * kakao channel
    :
    
    * homepage and menu update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * ui update
    
    * home dialog bug fix
    
    * update
    
    * update

--- lib/main.dart
+++ lib/main.dart
@@ -23,6 +23,7 @@ import 'package:phishing_kat_pluss/splash/network_error.dart';
 import 'package:phishing_kat_pluss/splash/splash_screen.dart';
 import 'package:phishing_kat_pluss/splash/update_page.dart';
 import 'package:provider/provider.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
 
 
 import 'kat_pages/info_check_page.dart';
@@ -67,6 +68,7 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -75,10 +77,8 @@ class MyApp extends StatelessWidget {
      * 앱의 초기 데이터를 불러오는 시간을 벌기위한 Splash Screen 화면은 FutureBuilder로 구현.
      */
     return FutureBuilder(
-
       ///future: 앱의 초기 설정및 데이터를 불러오는 곳
         future: context.read<LaunchProvider>().Init(),
-
         /// future의 상태에 따라 보여주는 화면이 다르다.
         /// future를 기다리는 중이면 Splash화면을 보여준다.
         builder: (context, AsyncSnapshot snapshot) {
@@ -88,6 +88,7 @@ class MyApp extends StatelessWidget {
           // }
           ///future에서 데이터를 불러온 다음 home page로 이동
           if (snapshot.connectionState == ConnectionState.done) {
+            print(snapshot.data);
             return MaterialApp(
               title: 'PhishingKaT+s', // 앱 이름
               ///전체앱의 theme 설정
--- lib/splash/network_error.dart
+++ lib/splash/network_error.dart
@@ -1,3 +1,4 @@
+import 'package:connectivity/connectivity.dart';
 import 'package:flutter/material.dart';
 import 'package:phishing_kat_pluss/theme.dart';
 
@@ -6,37 +7,65 @@ class NetworkError extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child:
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset('assets/images/network_error.png', height: 105.4,),
-            const SizedBox(height: 20,),
-            const Text(
-              '네트워크가 불안정합니다',
-              style: TextStyle(fontSize: 20, color: Colors.black, fontWeight: FontWeight.w800),
-            ),
-            const SizedBox(height: 17,),
-            const Text(
-              '네트워크 연결에 실패했습니다\n 확인 후 다시 시도해주세요',
-              style: TextStyle(fontSize: 15, color: Colors.black),
-            ),
-            const SizedBox(height: 80,),
-            OutlinedButton(
-              child: const Text('다시 시도하기', style: TextStyle(fontSize: 16, color: Color(0xFF0473e1), fontWeight: FontWeight.w800),),
-              onPressed: () {},
-              style: OutlinedButton.styleFrom(
-                side: const BorderSide(color: Color(0xFF0372e0), width: 2),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                fixedSize: const Size(208, 50)
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/images/network_error.png',
+                height: 105.4,
               ),
-            ),
-          ],
+              const SizedBox(
+                height: 20,
+              ),
+              const Text(
+                '네트워크가 불안정합니다',
+                style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.black,
+                    fontWeight: FontWeight.w800),
+              ),
+              const SizedBox(
+                height: 17,
+              ),
+              const Text(
+                '네트워크 연결에 실패했습니다\n 확인 후 다시 시도해주세요',
+                style: TextStyle(fontSize: 15, color: Colors.black),
+              ),
+              const SizedBox(
+                height: 80,
+              ),
+              OutlinedButton(
+                child: const Text(
+                  '다시 시도하기',
+                  style: TextStyle(
+                      fontSize: 16,
+                      color: Color(0xFF0473e1),
+                      fontWeight: FontWeight.w800),
+                ),
+                onPressed: () async {
+                  var connectivityResult =
+                      await (Connectivity().checkConnectivity());
+                  if (connectivityResult == ConnectivityResult.none) {
+                    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                        content: Text("네트워크 연결에 실패했습니다."),
+                        duration: Duration(seconds: 2)));
+                  } else if (connectivityResult == ConnectivityResult.wifi) {
+                    // I am connected to a wifi network.
+                  }
+                },
+                style: OutlinedButton.styleFrom(
+                    side: const BorderSide(color: Color(0xFF0372e0), width: 2),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    fixedSize: const Size(208, 50)),
+              ),
+            ],
+          ),
         ),
       ),
     );
